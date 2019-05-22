a95d8b1676c7bae9b44d086e3e6c388ccc77dad6
gergelyk <grzegorz@krason.me>
2019-05-23 01:46:49 +0200

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Grzegorz Krasoń'
 author = 'Grzegorz Krasoń'
 
 # The short X.Y version
-version = '0.3'
+version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.3.4'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
